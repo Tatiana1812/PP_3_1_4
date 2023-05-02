4657689e52f4f0716264d041616cb6c1117f5b98
Tatiana1812 <79156300+Tatiana1812@users.noreply.github.com>
2023-05-02 20:48:35 +0300

    Add files via upload

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/IndexController.java
@@ -0,0 +1,13 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    public String Index() {
+        return  "redirect:/login";
+    }
+}
